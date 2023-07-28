a950bf7f7c28c87d81db049e0187157e980e654b
Reval Fahmi <reval.fahmi42@gmail.com>
2023-07-28 21:28:45 +0700

    Authentication

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HakAksesController;
+use App\Http\Controllers\PenggunaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [DashboardController::class, 'index']);
 
 Route::resource('hak-akses', HakAksesController::class);
+
+Route::resource('pengguna', PenggunaController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
